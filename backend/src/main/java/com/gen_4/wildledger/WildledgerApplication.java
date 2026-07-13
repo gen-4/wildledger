305edc9d2ea57603157c8c514390ec7fe3d9a223
Anton <gendra.job@gmail.com>
2026-07-13 21:37:41 +0200

    feat: Create project skeleton

---
+++ backend/src/main/java/com/gen_4/wildledger/WildledgerApplication.java
@@ -0,0 +1,13 @@
+package com.gen_4.wildledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildledgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildledgerApplication.class, args);
+	}
+
+}
