7aa2bbf30c362938f3c0ae50ab33ac2c729bfba9
Anton <gendra.job@gmail.com>
2026-07-18 11:29:02 +0200

    feat: Add input validation to Register.tsx

--- frontend/src/components/auth/Register.tsx
+++ frontend/src/components/auth/Register.tsx
@@ -1,21 +1,25 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useState, useEffect } from "react";
+import { useState, useEffect, type ChangeEvent, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { register } from "@/components/auth/slices/authSlice";
 import { type AppDispatch } from "@/store";
 import { addMessage } from "@/store/appSlice";
 import { MessageType } from "@/store/types";
-import { isAuthenticatedSelector } from "@/components/auth/selectors";
+import { isAuthenticatedSelector, isLoadingSelector } from "@/components/auth/selectors";
 import { Button } from "@/components/common";
 
 import styles from '@/components/auth/styles/authentication.module.css';
 
-function Register() { // TODO: Doube password. Controll password has more than 6 characters
+function Register() {
     const navigate = useNavigate();
     const isAuthenticated = useSelector(isAuthenticatedSelector);
+    const isLoading = useSelector(isLoadingSelector);
     const [ username, setUsername ] = useState("");
     const [ password, setPassword ] = useState("");
+    const [ secondPassword, setSecondPassword ] = useState("");
+    const [ passwordWarning, setPasswordWarning ] = useState("");
+    const [ secondPasswordWarning, setSecondPasswordWarning ] = useState("");
     const dispatch: AppDispatch = useDispatch();
 
     useEffect(() => {
@@ -24,7 +28,11 @@ function Register() { // TODO: Doube password. Controll password has more than 6
         }
     }, [isAuthenticated, navigate]);
 
-    const onSubmitClick = async () => 
+    const onSubmitClick = async () => {
+        if (isLoading) {
+            return;
+        }
+
         dispatch(register({ username, password })).unwrap()
         .then(() => 
             dispatch(addMessage({
@@ -42,6 +50,44 @@ function Register() { // TODO: Doube password. Controll password has more than 6
                 autoDismiss: true,
                 dismissing: false
             })));
+    }
+
+    const disabled = useMemo(() => {
+        return !(
+            username.length > 0 &&
+            password.length > 5 &&
+            password === secondPassword
+        );
+    }, [username, password, secondPassword]);
+
+    const onPasswordChange = (e: ChangeEvent<HTMLInputElement>)  => {
+        setPassword(e.target.value);
+        if (e.target.value.length > 5 || e.target.value.length === 0) {
+            setPasswordWarning("");
+
+        } else {
+            setPasswordWarning("Password must be at least 6 characters long");
+        }
+        validateSecondPassword(secondPassword, e.target.value);
+    };
+
+    const validateSecondPassword = (value: string, password: string) => {
+        if (value === password || value.length === 0) {
+            setSecondPasswordWarning("");
+
+        } else {
+            setSecondPasswordWarning("Reentered password must be the same as first password");
+        }
+    };
+
+    const onSecondPasswordChange = (e: ChangeEvent<HTMLInputElement>)  => {
+        setSecondPassword(e.target.value);
+        validateSecondPassword(e.target.value, password);
+    };
+
+    const onUsernameChange = (e: ChangeEvent<HTMLInputElement>)  => {
+        setUsername(e.target.value);
+    };
 
     return (
         <div className={ styles.authCard }>
@@ -49,17 +95,26 @@ function Register() { // TODO: Doube password. Controll password has more than 6
                 className={ styles.input }
                 type="text" 
                 value={ username } 
-                onChange={(e) => setUsername(e.target.value)} 
+                onChange={ onUsernameChange } 
                 placeholder="username" 
             />
             <input 
-                className={ styles.input }
+                className={ `${styles.input} ${styles.withWarning}` }
                 type="password" 
                 value={ password } 
-                onChange={(e) => setPassword(e.target.value)} 
+                onChange={ onPasswordChange } 
                 placeholder="password" 
             />
-            <Button text="Submit" onClick={() => onSubmitClick()} cover />
+            <span className={ styles.warning }>{ passwordWarning }</span>
+            <input 
+                className={ `${styles.input} ${styles.withWarning}` }
+                type="password" 
+                value={ secondPassword } 
+                onChange={ onSecondPasswordChange } 
+                placeholder="reenter password" 
+            />
+            <span className={ styles.warning }>{ secondPasswordWarning }</span>
+            <Button text="Submit" onClick={() => onSubmitClick()} cover disabled={ disabled || isLoading } />
         </div>
     );
 };
--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -1,8 +1,13 @@
 import styles from '@/components/common/styles/button.module.css';
 
-const Button = ({ text, onClick, cover }: { text: string, onClick?: () => void, cover?: boolean}) => {
+const Button = ({ text, onClick, cover, disabled }: 
+    { text: string, onClick?: () => void, cover?: boolean, disabled?: boolean}) => {
     return (
-        <button className={ `${styles.button} ${cover && styles.cover}` } onClick={ onClick } >
+        <button 
+            className={ `${styles.button} ${cover && styles.cover} ${disabled && styles.disabled}` } 
+            onClick={ onClick } 
+            disabled={ disabled } 
+        >
             { text }
         </button>
     );
