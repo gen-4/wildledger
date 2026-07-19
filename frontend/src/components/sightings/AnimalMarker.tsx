bdd78b7429254d66b0b35aec06e94ab8351a36bd
Anton <gendra.job@gmail.com>
2026-07-19 11:14:38 +0200

    feat: Add AnimalMarkers.tsx. Sightings.tsx passes markers to map

---
+++ frontend/src/components/sightings/AnimalMarker.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useMemo, useRef, useState } from "react";
+import { Marker, Tooltip, useMap } from "react-leaflet";
+import { Icon, type IconOptions } from "leaflet";
+
+import type { SightingMarker } from '@/components/sightings/types';
+
+import '@/components/sightings/styles/leaflet.css'
+
+const AnimalMarker = ({ id, name, location, draggable }: SightingMarker) => {
+    const [position, setPosition] = useState(location);
+    const markerRef = useRef<L.Marker | null>(null);
+    const map = useMap();
+    const [zoom, setZoom] = useState(map.getZoom());
+    const iconSize: [number, number] = zoom <= 10 ? [50, 48] : [100, 95];
+    const tooltipOffset: [number, number] = zoom <= 10 ? [0, -30] : [0, -60];
+
+    useEffect(() => {
+        const onZoom = () => setZoom(map.getZoom());
+        map.on('zoomend', onZoom);
+        return () => { map.off('zoomend', onZoom); };
+    }, [map]);
+
+    const icon: IconOptions = {
+        iconUrl: 'fluke.svg',
+        iconSize
+    };
+
+    const eventHandlers = useMemo(() => ({
+            dragend() {
+                const marker = markerRef.current
+                if (marker != null) {
+                    setPosition(marker.getLatLng())
+                }
+            },
+        }), []
+    );
+
+    return (
+        <Marker
+            key={ id }
+            draggable={ draggable }
+            eventHandlers={ eventHandlers }
+            position={ position }
+            ref={ markerRef }
+            icon={ new Icon(icon) }
+        >
+            <Tooltip key={ `${zoom}-${id}` } direction="top" offset={ tooltipOffset } opacity={ 1 } permanent>
+                <span>#{ id }</span>
+                <span className="tooltip-title">{ name }</span>
+            </Tooltip>
+        </Marker>
+    );
+};
+
+export default AnimalMarker;
--- frontend/src/components/sightings/Sightings.tsx
+++ frontend/src/components/sightings/Sightings.tsx
@@ -1,58 +1,19 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import Map from '@/components/sightings/Map';
+import type { SightingMarker } from '@/components/sightings/types';
 
-import 'leaflet/dist/leaflet.css';
-
-import type { AppDispatch } from "@/store";
-import { addMessage } from "@/store/appSlice";
-import { MessageType } from "@/store/types";
-
-import mapStyles from '@/components/sightings/styles/map.module.css';
-
-const SetView = () => {
-    const map = useMap();
-    const dispatch: AppDispatch = useDispatch();
-
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(
-            (position) => map.setView([position.coords.latitude, position.coords.longitude], 13),
-            () => {
-                dispatch(addMessage({
-                    id: '',
-                    type: MessageType.INFO,
-                    message: "Using default location",
-                    autoDismiss: true,
-                    dismissing: false
-                }));
-                map.setView([42.88075187924244, -8.544497134456442], 13);
-            },
-            { enableHighAccuracy: true, timeout: 10000, maximumAge: 300000 }
-        );
-    }, [map, dispatch]);
-    
-    return null;
-};
+const markers: Array<SightingMarker> = [
+    {
+        id: 1,
+        name: "Eli's whale",
+        location: { lat: 42.88075187924244, lng: -8.544497134456442  },
+        draggable: true
+    }
+]
 
 const Sightings = () => {
     return (
-        <MapContainer 
-            center={ [42.88075187924244, -8.544497134456442] } 
-            minZoom={ 3 } 
-            maxZoom={ 18 } // With this Tile it has to be 18. Any other would be 19
-            className={ mapStyles.map } 
-        >
-            <TileLayer 
-                attribution={'&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver ' +
-                    '(Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">' + 
-                    'Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">' +
-                    'OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">' +
-                    'OpenStreetMap</a> contributors'}
-                url="https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.jpg" 
-            />
-            <SetView />
-        </MapContainer>
-    )
+        <Map markers={ markers } />
+    );
 };
 
 export default Sightings;
